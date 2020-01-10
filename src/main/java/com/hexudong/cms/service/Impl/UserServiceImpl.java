75d99a151924ab6f3c06b4cb47ad2818e2253e02
hexudong666 <1531422161@qq.com>
2020-01-10 20:06:46 +0800

    再次提交,基本功能

--- src/main/java/com/hexudong/cms/service/Impl/UserServiceImpl.java
+++ src/main/java/com/hexudong/cms/service/Impl/UserServiceImpl.java
@@ -1,11 +1,40 @@
 package com.hexudong.cms.service.Impl;
 
-import org.springframework.stereotype.Service;
+import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.hexudong.cms.common.CmsMd5Util;
+import com.hexudong.cms.dao.UserDao;
+import com.hexudong.cms.pojo.User;
 import com.hexudong.cms.service.UserService;
 
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService{
+	@Autowired
+	private UserDao userDao;
 
+	@Override
+	public boolean register(User user) {
+		/** 设置密码 **/
+		user.setPassword(CmsMd5Util.md5(user.getPassword()));
+		user.setLocked(0);
+		user.setScore(0);
+		user.setRole("0");
+		user.setCreate_time(new Date());
+		user.setUpdate_time(new Date());
+		return userDao.insert(user)>0;
+	}
+
+	@Override
+	public User getByUsername(String userName) {
+		return userDao.selectByUsername(userName);
+	}
+
+	@Override
+	public boolean locked(String userName) {
+		User userInfo = userDao.selectByUsername(userName);
+		return userInfo.getLocked()==1;
+	}
 	
 }
--- src/main/java/com/hexudong/cms/service/UserService.java
+++ src/main/java/com/hexudong/cms/service/UserService.java
@@ -1,5 +1,34 @@
 package com.hexudong.cms.service;
 
-public interface UserService {
+import com.hexudong.cms.pojo.User;
 
+public interface UserService {
+	/**
+	 * @Title: register   
+	 * @Description: �û�ע��   
+	 * @param: @param user
+	 * @param: @return      
+	 * @return: boolean      
+	 * @throws
+	 */
+	public boolean register(User user);
+	
+	/**
+	 * @Title: getByUsername   
+	 * @Description: ���ݵ�¼����ѯ�û�   
+	 * @param: @param userName
+	 * @param: @return      
+	 * @return: User      
+	 * @throws
+	 */
+	public User getByUsername(String userName);
+	/**
+	 * @Title: locked   
+	 * @Description: �Ƿ����   
+	 * @param: @param userName
+	 * @param: @return      
+	 * @return: boolean      
+	 * @throws
+	 */
+	public boolean locked(String userName);
 }
